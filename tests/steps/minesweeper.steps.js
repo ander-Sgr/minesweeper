b5d461a5ad84c3042eb93dc7a253c504f79f65e9
Travelport <Travelport@32SZGH2.galileo.corp.lcl>
2022-09-23 14:07:10 +0200

    try to do some steps

--- tests/steps/minesweeper.steps.js
+++ tests/steps/minesweeper.steps.js
@@ -6,3 +6,35 @@ Given('a user opens the app', async () => {
 	await page.goto(url);
 });
 
+
+async function getNumberRows() {
+	const board = page.locator('data-testid=dashboard-minesweeper')
+	let numOfRows = 0;
+	while (i < board.length) {
+		numOfRows ++;
+	}
+	console.log(numOfRows);
+	return numOfRows;
+	
+}
+
+async function getNumberColumns(numOf) {
+	/*const board = await page.locator('data-testid=dashboard-minesweeper')
+	let i = 0;
+	while (i < board.length) {
+		i++;
+	}
+	return i;*/
+}
+
+Given('the number of columns in the minefield should be: {string}', async function (string) {
+	const board = page.locator('data-testid=dashboard-minesweeper')
+
+	let numOfRows = getNumberRows();
+	expect(numOfRows).toBe(string);
+});
+
+Given('the number of rows in the minefield should be: {string}', function (string) {
+	// Write code here that turns the phrase above into concrete actions
+	return 'pending';
+});
