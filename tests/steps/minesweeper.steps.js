60545b48111a0d0bb1e914efcdd81386f357d0cf
Anderson Estrella <andersonbarreto2001@gmail.com>
2022-09-29 12:52:11 +0200

    try to do a test

--- tests/steps/minesweeper.steps.js
+++ tests/steps/minesweeper.steps.js
@@ -2,39 +2,28 @@ const { Given, When, Then } = require('@cucumber/cucumber');
 const { expect } = require('@playwright/test');
 const url = '127.0.0.1:5500/minesweeper/src/index.html';
 
-Given('a user opens the app', async () => {
-	await page.goto(url);
-});
+async function cellClick(cellId) {
+	await page.click(`[data-testid="${cellId}"]`, { force: true });
+}
 
-
-async function getNumberRows() {
-	const board = page.locator('data-testid=dashboard-minesweeper')
-	let numOfRows = 0;
-	while (i < board.length) {
-		numOfRows ++;
-	}
-	console.log(numOfRows);
-	return numOfRows;
+async function getLengthTable() {
+	const table = await page.locator('data-testid=dashboard-minesweeper');
 	
 }
 
-async function getNumberColumns(numOf) {
-	/*const board = await page.locator('data-testid=dashboard-minesweeper')
-	let i = 0;
-	while (i < board.length) {
-		i++;
-	}
-	return i;*/
+
+Given('a user opens the app', async () => {
+	await page.goto(url);
 }
 
-Given('the number of columns in the minefield should be: {string}', async function (string) {
-	const board = page.locator('data-testid=dashboard-minesweeper')
-
-	let numOfRows = getNumberRows();
-	expect(numOfRows).toBe(string);
-});
-
-Given('the number of rows in the minefield should be: {string}', function (string) {
+/*
+Given('the number of columns in the minefield should be: {string}', async (string) => {
 	// Write code here that turns the phrase above into concrete actions
 	return 'pending';
 });
+
+Given('the number of rows in the minefield should be: {string}', async (string) => {
+	// Write code here that turns the phrase above into concrete actions
+	return 'pending';
+});
+*/
